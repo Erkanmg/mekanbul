fca1470df237cad5b367a6091b834ed3a1024821
Erkan Mert Güngör <merterkan51@gmail.com>
2022-12-10 23:02:24 +0300

    odev8 branchi oluşturuldu

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 var createError = require('http-errors');
-//require('./app_server/models/db');
-require('./app_api/models/db');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('./app_api/models/db');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
+var session = require('express-session');
+
 var app = express();
 
+app.use(session({
+  secret: "gizli",
+  cookie:{ maxAge: 1000*60*60*24},
+  resave:true,
+  saveUninitialized:true
+}));
+
 // view engine setup
-app.set('views', path.join(__dirname,'app_server','views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
